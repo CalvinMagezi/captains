203ef4aedb7bd458788fdb2477887805c90ae990
Calvin Magezi <30309553+CalvinMagezi@users.noreply.github.com>
2021-06-14 06:07:26 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/dashboard');
+    return redirect('/dashboard');
 });
 Route::get('/home', [App\Http\Controllers\AdminController::class, 'index'])->middleware('auth');
 Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
