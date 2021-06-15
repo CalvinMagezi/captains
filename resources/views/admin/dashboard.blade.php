b9f043e1dfdc08a01fb96a8a903c36283c9f30f4
Calvin Magezi <30309553+CalvinMagezi@users.noreply.github.com>
2021-06-15 14:30:26 +0300

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -132,7 +132,7 @@
                                                 <div class="card">
                                                     <div class="card-header">
                                                         <h5>Table Maps</h5>
-                                                        <span class="text-muted">View current outside table mapping and table status.<em style="color: red">Red</em> means inactive, <em style="color: green">Green</em> means active.</span>
+                                                        <span class="text-muted">View current table mapping and table status.<em style="color: red">Red</em> means inactive, <em style="color: green">Green</em> means active.</span>
                                                         <div class="card-header-right">
                                                             <ul class="list-unstyled card-option">
                                                                 <li><i class="fa fa fa-wrench open-card-option"></i></li>
@@ -144,9 +144,28 @@
                                                         </div>
                                                     </div>
                                                     <div class="card-block">
-                                                        <div id="mapping-container" class="mapping-container" >
+                                                        <div id="mapping-container" class="mapping-container mx-auto" >
                                                             <section id="outside-tables">
-      
+                                                                <div class="balcony text-center">View Of Park</div>
+                                                                <div class="long-table-1 text-center">Long Table 1</div>
+                                                                <div class="bar text-center">Cocktail Bar</div>
+                                                                <div class="long-table-2 text-center">Long Table 2</div>
+                                                                <div class="dj text-center"> DJ Stand</div>
+                                                                <div class="kitchen text-center">K I T C H E N</div>
+                                                                <div class="page-line-l"></div>
+                                                                <div class="page-line-r"></div>
+                                                                <div class="vip-1 text-center">Prive' 1</div>
+                                                                <div class="vip-2 text-center">Prive' 2</div>
+                                                                <div class="bar-2 text-center">Bar</div>
+                                                                <div class="station text-center">Station</div>
+                                                                <div class="bar-3 text-center">Main Bar</div>
+                                                                <div class="dj-2 text-center">DJ Stand</div>
+                                                                {{-- <div class="curtain-1"></div>
+                                                                <div class="curtain-2"></div>
+                                                                <div class="curtain-3"></div>
+                                                                <div class="curtain-4"></div>
+                                                                <div class="curtain-5"></div>
+                                                                <div class="curtain-6"></div> --}}
                                                             </section>                                                      
                                                         </div>
                                                     </div>
