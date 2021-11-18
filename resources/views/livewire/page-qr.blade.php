d440566a8744614c74e6cd2058e759fbf6b07dc7
Calvin Magezi <30309553+CalvinMagezi@users.noreply.github.com>
2021-11-18 16:04:28 +0300

    Update page-qr.blade.php

--- resources/views/livewire/page-qr.blade.php
+++ resources/views/livewire/page-qr.blade.php
@@ -1,4 +1,9 @@
 <div>
+    <div class="text-center row justify-content-center">
+        <div class="col-4">
+            <img src="{{ asset('img/logo.svg') }}" style="width:14rem;" alt="">
+        </div>
+    </div>
     @if ($table_chosen)
     @if ($table_chosen->managed_by != 'free')
         <h1>Table Is Managed By: {{ $table_chosen->managed_by }}</h1>
@@ -7,9 +12,14 @@
         <h1>This table is: {{ $table_chosen->managed_by }}</h1>
         <button class="btn btn-success btn-lg">Book Now {{ $table_chosen->managed_by }}</button>
     @endif
-    {{-- <div class="row justi">
-        <div class="col-4"></div>
-    </div> --}}
+    <div class="text-center row justify-content-center">
+        <div class="col-4">
+            <a target="_blank" href="https://captain-s-terrace.web.app/menu.html"class="btn btn-primary btn-lg" >
+                <h1>Browse Our Menu </h1>
+            </a>
+        </div>
+    </div>
+
 
     @endif
 </div>
